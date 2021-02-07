835c32def9c443c88f8cdee90e7d64bdff981066
Emilio Resende <emilio.resende@faculdadeimpacta.com.br>
2021-02-07 18:26:33 -0300

    initial commit

---
+++ src/main/java/fit/core/Airplane.java
@@ -0,0 +1,24 @@
+package fit.core;
+
+public class Airplane {
+  public void start() {
+    System.out.println("The airplane is starting ...");
+  }
+
+  public void stop() {
+    System.out.println("The airplane is stopping ...");
+  }
+
+  // non-sense...
+  public void doPredictiveMaintenance() {
+    System.out.println("The airplane is doing maintenance ???");
+  }
+
+  public void taxi() {
+    System.out.println("The airplane is taxiing ???");
+  }
+
+  public void wash() {
+    System.out.println("The airplane is washing ???");
+  }
+}
